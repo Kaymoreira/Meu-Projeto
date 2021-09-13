677ec0006c421dbbce62d4d7c4f1d44b4b2f0953
spwn <kayque221023@gmail.com>
2021-09-13 10:40:41 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Home from '../src/screens/Home'
+import Home from '../src/screens/Detail'
+
+const App = () => {
+    return (
+        <div>
+        <Home />
+        <Detail />
+        </div>
+        );
+};
+
+export default App;
+
+//parei no 9:44 do video criando uma aplicacao react do broga
